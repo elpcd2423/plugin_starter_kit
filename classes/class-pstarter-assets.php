43670fd697f409166834450f3c1645b1a72cbcd5
elpcd2423 <elpcd2423@gmail.com>
2023-07-26 14:20:46 +0200

    Changed priority to script_loader_src

--- classes/class-pstarter-assets.php
+++ classes/class-pstarter-assets.php
@@ -19,7 +19,7 @@ if ( ! class_exists( 'PStarter_Plugin_Assets' ) ){
                 }
             
                 return $src;
-            }, 99, 2 );
+            }, 98, 2 );
             add_action( 'wp_enqueue_scripts', [ $this, 'pstarter_enqueue_backend_scripts_and_styles' ] );
         }
 
